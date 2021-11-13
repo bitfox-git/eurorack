6bb476a7b5b5c1fbbbe2a0b2e3cc9094ed2282d3
Chris Vos <Chris@lucrasoft.nl>
2021-11-13 22:01:37 +0100

    vscode .mx add

---
+++ screen/code/build/ips_font.d
@@ -0,0 +1,3 @@
+build/ips_font.o: Src/ips_font.c Inc/ips_font.h
+
+Inc/ips_font.h:
---
+++ screen/code/build/kiss_fftr.d
@@ -0,0 +1,8 @@
+build/kiss_fftr.o: Src/kiss_fftr.c Inc/kiss_fftr.h Inc/kiss_fft.h \
+ Inc/_kiss_fft_guts.h
+
+Inc/kiss_fftr.h:
+
+Inc/kiss_fft.h:
+
+Inc/_kiss_fft_guts.h:
